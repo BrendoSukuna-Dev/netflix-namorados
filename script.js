b56b14204435fc59b4792d6abd0265523c24213d
Brendo Santos <brendo.engdev@gmail.com>
2026-06-08 11:14:29 -0300

    Retirando o modo autoplay

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  slidesPerView: 2,
-  spaceBetween: 10,
-  loop: true,
-  breakpoints: {
-    600: {
-      loop: true,
-      slidesPerView: 3,
-      spaceBetween: 15,
+// Seleciona todos os carrosséis com a classe .swiper na página
+const carrosseis = document.querySelectorAll(".swiper");
+
+// Inicializa o Swiper para cada um deles separadamente
+carrosseis.forEach((carrossel) => {
+  new Swiper(carrossel, {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 10,
+
+    // O bloco 'autoplay' foi removido daqui para parar a rolagem automática!
+
+    breakpoints: {
+      600: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      900: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
     },
-    900: {
-      loop: true,
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-  },
+  });
 });
